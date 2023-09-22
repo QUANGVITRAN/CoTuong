d175ff03bee727882a3adcf43ded17adabfed790
TranQuangVi <112139716+TranQuangVi@users.noreply.github.com>
2023-09-22 16:01:36 +0700

    Update User.cs
    add row password

--- Libs/Entity/User.cs
+++ Libs/Entity/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,6 @@ namespace Libs.Entity
         public Guid Id { get; set; }
         public string UserName { get; set; }
         public Guid RoomId { get; set; }
+        public string Password { get; set; }
     }
 }
